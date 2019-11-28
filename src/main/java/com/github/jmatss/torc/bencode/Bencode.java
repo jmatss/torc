e0aca9a2c2c2026abc2f0625baf992f82d617915
Jonathan Matsson <jmatss@kth.se>
2019-11-28 17:47:47 +0100

    Change to using CharBuffer instead of ByteBuffer

--- src/main/java/com/github/jmatss/torc/bencode/Bencode.java
+++ src/main/java/com/github/jmatss/torc/bencode/Bencode.java
@@ -4,32 +4,34 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Bencode {
     public static final String ENCODING = "utf-8";
     public static final char STRING_SEPARATOR = ':';
-    private ByteBuffer buffer;
+    private CharBuffer buffer;
 
-    public Bencode(ByteBuffer buffer) {
+    public Bencode(CharBuffer buffer) {
         this.buffer = buffer;
     }
 
-    public Bencode(byte[] fileContent) {
-        this(ByteBuffer.allocate(fileContent.length).put(fileContent));
+    public Bencode(char[] fileContent) {
+        this(CharBuffer.allocate(fileContent.length).put(fileContent));
         this.buffer.rewind();
     }
 
     public Bencode(File file) throws IOException {
-        this(Files.readAllBytes(file.toPath()));
+        this(
+                new String(
+                        Files.readAllBytes(file.toPath()),
+                        ENCODING
+                ).toCharArray()
+        );
     }
 
-    public BencodeResult<?> getNext() throws EOFException, BencodeException, UnsupportedEncodingException {
+    public BencodeResult getNext() throws EOFException, BencodeException, UnsupportedEncodingException {
         BencodeType type = getNextType();
         BencodeResult<?> result;
 
@@ -58,64 +60,65 @@ public class Bencode {
             throw new EOFException();
 
         int oldPosition = this.buffer.position();
-        BencodeType type = BencodeType.valueOf(this.buffer.getChar());
+        char typeChar = this.buffer.get();
+        BencodeType type = BencodeType.valueOf(typeChar);
         if (type == null)
-            throw new BencodeException("Incorrect format of bencoded data.");
+            throw new BencodeException("Incorrect format of bencoded data. Expected BencodeType, got: " + typeChar);
 
         // Reset position so that the next "read" of the buffer starts at the correct position.
         this.buffer.position(oldPosition);
         return type;
     }
 
-    public String getString() throws BencodeException, UnsupportedEncodingException {
+    public String getString() throws BencodeException {
         StringBuilder sb = new StringBuilder();
 
         char current;
-        while ((current = buffer.getChar()) != STRING_SEPARATOR) {
+        while ((current = buffer.get()) != STRING_SEPARATOR) {
             if (!Character.isDigit(current))
                 throw new BencodeException("Received non digit character while parsing string length: " + current);
             sb.append(current);
         }
 
         // TODO: Make sure that the length isn't a weird value ex. extremely large.
-        int stringLength = Integer.parseInt(sb.reverse().toString());
-        byte[] str = new byte[stringLength];
+        int stringLength = Integer.parseInt(sb.toString());
+        char[] str = new char[stringLength];
         this.buffer.get(str);
 
-        return new String(str, ENCODING);
+        return new String(str);
     }
 
-    public long getNumber() {
+    public Long getNumber() {
         this.buffer.position(this.buffer.position() + 1);   // "Remove" 'i'.
 
         StringBuilder sb = new StringBuilder();
         char current;
-        while (Character.isDigit(current = buffer.getChar())) {
+        while (Character.isDigit(current = buffer.get())) {
             sb.append(current);
         }
+        // The last iteration of the while loop "removes" the 'e'.
 
-        this.buffer.position(this.buffer.position() + 1);   // "Remove" 'e'.
-        return Integer.parseInt(sb.toString());
+        return Long.parseLong(sb.toString());
     }
 
-    public Map<String, Object> getDictionary() throws EOFException, BencodeException, UnsupportedEncodingException {
+    public Map<String, BencodeResult> getDictionary() throws EOFException, BencodeException, UnsupportedEncodingException {
         this.buffer.position(this.buffer.position() + 1);   // "Remove" 'd'.
-        Map<String, Object> map = new HashMap<>();
+        Map<String, BencodeResult> map = new HashMap<>();
 
         BencodeType nextKeyType;
         while ((nextKeyType = getNextType()) != BencodeType.END) {
             if (nextKeyType != BencodeType.STRING)
                 throw new BencodeException("Received a non String key for a dictionary.");
-            map.put(getString(), getNext().getValue());
+            map.put(getString(), getNext());
         }
 
         this.buffer.position(this.buffer.position() + 1);   // "Remove" 'e'.
         return map;
     }
 
-    public List<Object> getList() throws EOFException, BencodeException, UnsupportedEncodingException {
+    public List<BencodeResult> getList() throws EOFException, BencodeException, UnsupportedEncodingException {
         this.buffer.position(this.buffer.position() + 1);   // "Remove" 'l'.
-        List<Object> list = new ArrayList<>();
+        List<BencodeResult> list = new ArrayList<>();
 
         while (getNextType() != BencodeType.END) {
             list.add(getNext());
