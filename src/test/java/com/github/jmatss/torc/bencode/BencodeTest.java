af8a53b9f2f60109c545d6a59643bc4bfdd5db82
Jonathan Matsson <jmatss@kth.se>
2019-11-29 23:21:09 +0100

    Update tests according to the changes in Bencode

--- src/test/java/com/github/jmatss/torc/bencode/BencodeTest.java
+++ src/test/java/com/github/jmatss/torc/bencode/BencodeTest.java
@@ -6,10 +6,12 @@ import java.io.EOFException;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
 
+import static com.github.jmatss.torc.bencode.Bencode.ENCODING;
 import static org.junit.jupiter.api.Assertions.*;
 
+// TODO: Use get"Structure"() instead of getNext() during tests(?)
 public class BencodeTest {
-    private <T> void runTest(char[] input, T[] expectedOutputs, BencodeType[] expectedTypes)
+    private <T> void runTest(byte[] input, T[] expectedOutputs, BencodeType[] expectedTypes)
     throws EOFException, BencodeException, UnsupportedEncodingException {
         if (expectedOutputs.length != expectedTypes.length)
             throw new IllegalArgumentException("expectedOutputs.length != expectedTypes.length");
@@ -26,7 +28,7 @@ public class BencodeTest {
 
     @Test
     public void testDecodeNumber() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = "i123e".toCharArray();
+        byte[] input = "i123e".getBytes();
         Long[] expectedOutputs = {123L};
         BencodeType[] expectedTypes = {BencodeType.NUMBER};
 
@@ -35,7 +37,7 @@ public class BencodeTest {
 
     @Test
     public void testDecodeTwoNumbers() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = ("i123e" + "i45e").toCharArray();
+        byte[] input = ("i123e" + "i45e").getBytes();
         Long[] expectedOutputs = {123L, 45L};
         BencodeType[] expectedTypes = {BencodeType.NUMBER, BencodeType.NUMBER};
 
@@ -44,8 +46,8 @@ public class BencodeTest {
 
     @Test
     public void testDecodeString() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = "4:test".toCharArray();
-        String[] expectedOutputs = {"test"};
+        byte[] input = "4:test".getBytes();
+        BencodeString[] expectedOutputs = {new BencodeString("test", ENCODING)};
         BencodeType[] expectedTypes = {BencodeType.STRING};
 
         runTest(input, expectedOutputs, expectedTypes);
@@ -53,8 +55,11 @@ public class BencodeTest {
 
     @Test
     public void testDecodeTwoStrings() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = ("4:test" + "10:0123456789").toCharArray();
-        String[] expectedOutputs = {"test", "0123456789"};
+        byte[] input = ("4:test" + "10:0123456789").getBytes();
+        BencodeString[] expectedOutputs = {
+                new BencodeString("test", ENCODING),
+                new BencodeString("0123456789", ENCODING)
+        };
         BencodeType[] expectedTypes = {BencodeType.STRING, BencodeType.STRING};
 
         runTest(input, expectedOutputs, expectedTypes);
@@ -62,7 +67,7 @@ public class BencodeTest {
 
     @Test
     public void testDecodeEmptyList() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = "le".toCharArray();
+        byte[] input = "le".getBytes();
         // expectedOutput => empty list;
         BencodeType expectedType = BencodeType.LIST;
 
@@ -76,9 +81,9 @@ public class BencodeTest {
 
     @Test
     public void testDecodeListOfOneString() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = ("l" + "4:test" + "e").toCharArray();
+        byte[] input = ("l" + "4:test" + "e").getBytes();
         int expectedListSize = 1;
-        String expectedOutputInner = "test";
+        BencodeString expectedOutputInner = new BencodeString("test", ENCODING);
         BencodeType expectedType = BencodeType.LIST;
         BencodeType expectedTypeInner = BencodeType.STRING;
 
@@ -96,7 +101,7 @@ public class BencodeTest {
 
     @Test
     public void testDecodeEmptyDictionary() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = ("de").toCharArray();
+        byte[] input = ("de").getBytes();
         // expectedOutput => empty map;
         BencodeType expectedType = BencodeType.DICTIONARY;
 
@@ -104,16 +109,16 @@ public class BencodeTest {
         BencodeResult actualResult = bencode.getNext();
 
         assertEquals(expectedType, actualResult.getType());
-        Map<String, BencodeResult> actualResultMap = (Map<String, BencodeResult>) actualResult.getValue();
+        Map<BencodeString, BencodeResult> actualResultMap = (Map<BencodeString, BencodeResult>) actualResult.getValue();
         assertTrue(actualResultMap.isEmpty());
     }
 
 
     @Test
     public void testDecodeDictionaryOfOneNumber() throws EOFException, BencodeException, UnsupportedEncodingException {
-        char[] input = ("d" + "3:key" + "i123e" + "e").toCharArray();
+        byte[] input = ("d" + "3:key" + "i123e" + "e").getBytes();
         int expectedMapSize = 1;
-        String expectedKey = "key";
+        BencodeString expectedKey = new BencodeString("key", ENCODING);
         long expectedValue = 123L;
         BencodeType expectedType = BencodeType.DICTIONARY;
         BencodeType expectedTypeValue = BencodeType.NUMBER;
@@ -122,7 +127,7 @@ public class BencodeTest {
         BencodeResult actualResult = bencode.getNext();
 
         assertEquals(expectedType, actualResult.getType());
-        Map<String, BencodeResult> actualResultMap = (Map<String, BencodeResult>) actualResult.getValue();
+        Map<BencodeString, BencodeResult> actualResultMap = (Map<BencodeString, BencodeResult>) actualResult.getValue();
         assertEquals(expectedMapSize, actualResultMap.size());
 
         assertTrue(actualResultMap.containsKey(expectedKey));
