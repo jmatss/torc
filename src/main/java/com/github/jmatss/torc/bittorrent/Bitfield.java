a9f8e50e859bb0ba51fa18c2ef680851f31facef
Jonathan Matsson <jmatss@kth.se>
2019-11-30 17:22:03 +0100

    Add outOfBounds check and amountOfPieces field

--- src/main/java/com/github/jmatss/torc/bittorrent/Bitfield.java
+++ src/main/java/com/github/jmatss/torc/bittorrent/Bitfield.java
@@ -6,25 +6,24 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Bitfield {
     private final Lock mutex;
     private final byte[] bitfield;
+    private final int amountOfPieces;
 
     public Bitfield(int amountOfPieces) {
         this.mutex = new ReentrantLock();
         this.bitfield = new byte[((amountOfPieces - 1) / 8) + 1];
-    }
-
-    public Bitfield(byte[] bitfield) {
-        this.mutex = new ReentrantLock();
-        this.bitfield = bitfield;
+        this.amountOfPieces = amountOfPieces;
     }
 
     // Sets the piece with "index" to 1.
     public synchronized void set(int index) {
+        outOfBoundsGuard(index);
         this.bitfield[byteIndex(index)] |= (1 << shiftAmount(index));
     }
 
     // Tries to set "index" to 1. If "index" currently is 0, set it to 1 and return "true".
     // Else, return false.
     public synchronized boolean trySet(int index) {
+        outOfBoundsGuard(index);
         if (!isSet(index)) {
             set(index);
             return true;
@@ -35,12 +34,18 @@ public class Bitfield {
 
     // Sets the piece with "index" to 0.
     public synchronized void clear(int index) {
+        outOfBoundsGuard(index);
         this.bitfield[byteIndex(index)] &= ~(1 << shiftAmount(index));
     }
 
     // Returns "true" if piece "index" contains a 1. Returns "false" if it contains a 0.
     public synchronized boolean isSet(int index) {
+        outOfBoundsGuard(index);
         return (this.bitfield[byteIndex(index)] & (1 << shiftAmount(index))) > 0;
+    }
+
+    public int getAmountOfPieces() {
+        return this.amountOfPieces;
     }
 
     // byteIndex is the index of the byte inside the "bitfield" array that contains the "real" index.
@@ -51,5 +56,12 @@ public class Bitfield {
     // shiftAmount represents how much the bit needs to be shifted left to get to the correct "bit index".
     private int shiftAmount(int index) {
         return 7 - (index % 8);
+    }
+
+    private void outOfBoundsGuard(int index) {
+        if (index >= this.amountOfPieces)
+            throw new IndexOutOfBoundsException("index >= amountOfPieces (" + index + " >= " + amountOfPieces + ")");
+        else if (index < 0)
+            throw new IndexOutOfBoundsException("index < 0 (" + index + " < 0)");
     }
 }
