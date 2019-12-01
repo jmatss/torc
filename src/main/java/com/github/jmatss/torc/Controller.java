15d9f5eb1f170f0a9b3bd862e73acfc6a658e813
Jonathan Matsson <jmatss@kth.se>
2019-12-01 19:24:40 +0100

    Make peerId non static

--- src/main/java/com/github/jmatss/torc/Controller.java
+++ src/main/java/com/github/jmatss/torc/Controller.java
@@ -12,17 +12,16 @@ import java.util.concurrent.Executors;
 public class Controller {
     // TODO: remove this temp download root path
     private static final String DOWNLOAD_ROOT_PATH = "";
-    private static byte[] peerId = null;
 
     private final ExecutorService executor;
     private final String rootPath;
+    private final byte[] peerId;
 
     Controller() {
         int processors = Runtime.getRuntime().availableProcessors();
         this.executor = Executors.newFixedThreadPool(processors);
-        if (peerId == null)
-            peerId = newPeerId();
         this.rootPath = DOWNLOAD_ROOT_PATH;
+        this.peerId = newPeerId();
     }
 
     public List<Runnable> shutdown() {
@@ -37,8 +36,8 @@ public class Controller {
         }
     }
 
-    public static byte[] getPeerId() {
-        return peerId;
+    public byte[] getPeerId() {
+        return this.peerId;
     }
 
     // Format of peer id: -<client id(2 bytes)><version(4 bytes)>-<12 random ascii numbers>
