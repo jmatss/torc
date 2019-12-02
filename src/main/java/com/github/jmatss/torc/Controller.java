58062e6b1f050535f316359ac0aff0a25aae8dc0
Jonathan Matsson <jmatss@kth.se>
2019-12-02 18:26:00 +0100

    Implement more controller logic

--- src/main/java/com/github/jmatss/torc/Controller.java
+++ src/main/java/com/github/jmatss/torc/Controller.java
@@ -1,11 +1,13 @@
 package com.github.jmatss.torc;
 
+import com.github.jmatss.torc.bittorrent.InfoHash;
 import com.github.jmatss.torc.bittorrent.Torrent;
+import com.github.jmatss.torc.util.concurrent.ComChannel;
+import com.github.jmatss.torc.util.concurrent.ComMessage;
 
 import java.nio.ByteBuffer;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,14 +16,36 @@ public class Controller {
     private static final String DOWNLOAD_ROOT_PATH = "";
 
     private final ExecutorService executor;
+
     private final String rootPath;
     private final byte[] peerId;
 
-    Controller() {
+    // This ComChannel is used to send messages between this controller and the torrent handlers asynchronously.
+    private final ComChannel torrentHandlersChannel;
+
+
+    private final BlockingQueue<ComMessage> sendToView;
+
+    // This channel will contain every message that is sent to this controller.
+    // That includes message from both the View and the torrent handlers.
+    private final BlockingQueue<ComMessage> receiver;
+
+    // List of all "active" torrents in the client.
+    private final Map<InfoHash, Torrent> torrents;
+
+    Controller(BlockingQueue<ComMessage> sendToView, BlockingQueue<ComMessage> receiver) {
         int processors = Runtime.getRuntime().availableProcessors();
         this.executor = Executors.newFixedThreadPool(processors);
+
         this.rootPath = DOWNLOAD_ROOT_PATH;
         this.peerId = newPeerId();
+
+        this.torrentHandlersChannel = new ComChannel();
+        this.sendToView = sendToView;
+        this.receiver = receiver;
+
+        this.torrents = getTorrentsFromDisk();
+        this.executor.submit(this::run);
     }
 
     public List<Runnable> shutdown() {
@@ -31,9 +55,18 @@ public class Controller {
         return this.executor.shutdownNow();
     }
 
-    public void start() {
+    public void run() {
         while (true) {
+
         }
+    }
+
+    public void addTorrent(String filename) {
+
+    }
+
+    public void removeTorrent(byte[] infoHash) {
+
     }
 
     public byte[] getPeerId() {
@@ -55,5 +88,11 @@ public class Controller {
 
     private List<Torrent> fetchTorrents() {
         return Collections.emptyList();
+    }
+
+    // TODO: implement logic for fetching torrent from previous session.
+    //  Currently only returns an empty HashMap.
+    private Map<InfoHash, Torrent> getTorrentsFromDisk() {
+        return new HashMap<>();
     }
 }
