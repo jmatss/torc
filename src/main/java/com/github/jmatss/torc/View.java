75c018b0ac5e25fa75d4819f4ab8450b4530129f
Jonathan Matsson <jmatss@kth.se>
2019-12-08 20:43:44 +0100

    Implement basic logic for View loop

--- src/main/java/com/github/jmatss/torc/View.java
+++ src/main/java/com/github/jmatss/torc/View.java
@@ -2,6 +2,7 @@ package com.github.jmatss.torc;
 
 import com.github.jmatss.torc.util.com.ComMessage;
 
+import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class View {
@@ -11,6 +12,21 @@ public class View {
         var recvFromController = new ArrayBlockingQueue<ComMessage>(QUEUE_SIZE);
         var sendToController = new ArrayBlockingQueue<ComMessage>(QUEUE_SIZE);
         Controller controller = new Controller(recvFromController, sendToController);
-        //controller.run();
+        controller.run();
+
+        while (true) {
+            String s;
+            try (var input = new Scanner(System.in)) {
+                s = input.nextLine();
+                if (s.isEmpty()) {
+                    continue;
+                }
+
+                String[] cmd = s.split(" ");
+                switch (cmd[0]) {
+
+                }
+            }
+        }
     }
 }
