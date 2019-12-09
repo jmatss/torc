d325294effa97fadccb2d20f13d6c6d4c6c35842
Jonathan Matsson <jmatss@kth.se>
2019-12-09 22:19:58 +0100

    Add QUEUE_SIZE to consts

--- src/main/java/com/github/jmatss/torc/TMP_CONST.java
+++ src/main/java/com/github/jmatss/torc/TMP_CONST.java
@@ -3,4 +3,5 @@ package com.github.jmatss.torc;
 public class TMP_CONST {
     public static final int SHA1_HASH_LENGTH = 20;
     public static final String ENCODING = "utf-8";
+    public static final int QUEUE_SIZE = 10;
 }
